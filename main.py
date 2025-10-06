fc915d25ec31d5c8ce97ae4b719b877d52a8c184
mehdi-mourzagh <mehdi.mourzagh@edu.esiee.fr>
2025-10-06 18:37:38 +0000

    tuples

--- main.py
+++ main.py
@@ -1,53 +1,60 @@
-#### Imports et définition des variables globales
 
-# Mandatory for the recursive solution to work on large inputs
-import sys
-sys.setrecursionlimit(2000)
+"""
+Module d'encodage de chaînes de caractères en liste de tuples
+(caractère, nombre d'occurrences consécutives).
+"""
+from typing import List, Tuple
 
-
-#### Fonctions secondaires
-
-
-def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
-
-    Args:
-        s (str): la chaîne de caractères à encoder
-
-    Returns:
-        list: la liste des tuples (caractère, nombre d'occurences)
+def artcode_i(s: str) -> List[Tuple[str, int]]:
     """
-    
-    # votre code ici
-
-    return [ ]
-
-
-def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
-
+    Encode une chaîne en liste de tuples (caractère, nombre d'occurrences consécutives).
+    Version itérative.
     Args:
-        s (str): la chaîne de caractères à encoder
-
+        s (str): chaîne à encoder
     Returns:
-        list: la liste des tuples (caractère, nombre d'occurences)
+        List[Tuple[str, int]]: liste de tuples (caractère, nombre d'occurrences)
     """
-    
-    # votre code ici
+    if not s:
+        return []
+    chars = [s[0]]
+    occs = [1]
+    k = 1
+    n = len(s)
+    while k < n:
+        if s[k] == s[k - 1]:
+            occs[-1] += 1
+        else:
+            chars.append(s[k])
+            occs.append(1)
+        k += 1
+    return list(zip(chars, occs))
 
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
+def artcode_r(s: str) -> List[Tuple[str, int]]:
+    """
+    Encode une chaîne en liste de tuples (caractère, nombre d'occurrences consécutives).
+    Version récursive.
+    Args:
+        s (str): chaîne à encoder
+    Returns:
+        List[Tuple[str, int]]: liste de tuples (caractère, nombre d'occurrences)
+    """
+    if not s:
+        return []
+    first = s[0]
+    i = 1
+    n = len(s)
+    while i < n and s[i] == first:
+        i += 1
+    return [(first, i)] + artcode_r(s[i:])
 
-    return []
-    
-
-#### Fonction principale
-
-
-def main():
-    print(artcode_i('MMMMaaacXolloMM'))
-    print(artcode_r('MMMMaaacXolloMM'))
+def main() -> None:
+    """
+    Fonction principale pour tester l'encodage itératif et récursif.
+    """
+    chaine = "MMMMaaacXolloMM"
+    print("Chaîne :", chaine)
+    print("Itératif :", artcode_i(chaine))
+    print("Récursif :", artcode_r(chaine))
 
 if __name__ == "__main__":
     main()
